01fc0766169e518edfc39b8dd5db9b1ba2ac6247
keidsondesigner <keidsondesigner@gmail.com>
2022-10-31 20:21:38 -0400

    primeira rota com prisma

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,30 @@
-import Fastify from "fastify";
+import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({
+	log: ['query']
+});
 
 async function bootstrap(){
 	const fastify = Fastify({
 		logger: true,
 	})
 
-	fastify.get('/pools/count', () => {
-		return { count: 3434}
+	fastify.get('/pools/count', async () => {
+		// findMany retorna todos os registros da tabela;
+		// const pools = await prisma.pool.findMany({
+		// 	where: {
+		// 		code: {
+		// 			startsWith: 'D'
+		// 		}
+		// 	}
+		// })
+
+		// return { pools };
+
+		// count() retorna apenas a aquiatidade;
+		const count = await prisma.pool.count()
+		return { count }
 	});
 
 	await fastify.listen({ port: 3333});
